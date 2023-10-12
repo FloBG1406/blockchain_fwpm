f4e708a9bbe303e3ebab477ffbde109f76493eda
FloBG1406 <127948435+FloBG1406@users.noreply.github.com>
2023-10-12 16:57:55 +0200

    Add main-method and getNameFromJson() and GSON and slf4j logger dependencies

---
+++ GSON/src/main/java/Main.java
@@ -0,0 +1,26 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.slf4j.*;
+public class Main {
+
+    private final static Logger logger = LoggerFactory.getLogger(Main.class);
+
+
+    public static void main(String[] args){
+
+        String json = "{\"name\": \"hendrik\"}";
+        logger.info("Application starts..");
+        System.out.println(getNameFromJson(json));
+        logger.info("Name: {}", getNameFromJson(json));
+    }
+
+    public static String getNameFromJson(String json){
+        JsonParser parser = new JsonParser();
+        JsonElement element = parser.parse(json);
+        JsonObject object = element.getAsJsonObject();
+        String name = object.get("name").getAsString();
+
+        return name;
+    }
+}
